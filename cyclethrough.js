d065b10e9e35220d92eea03f7f5317471b36c8be
Jordan Louis <jdmlouis@gmail.com>
2014-06-12 15:39:21 -0400

    Comments added for functional clarity.

--- cyclethrough.js
+++ cyclethrough.js
@@ -1,36 +1,40 @@
-searchString = "serv";
+// A simple JS script to sift through all elements on page, 
+// looking for all IDs that match // a given search string.
 
-findElements();
+searchString = "serv"; // What string are we looking for inside our id attributes?
+
+findElements(); 
 
 clickLoop();
 
 function findElements() {
-	i = document.links.length;
+	i = document.links.length; // Get number of links in document.
 	n = 0;
 	resultarr = new Array
 	for (rev = 0; rev < i; rev++){
 		if (!document.links[rev].id){
-			console.log("nope (no id)\n");
+			console.log("nope (no id)\n"); // If there's no ID, tell the console.
 		}
 		else {
 			if(document.links[rev].id.indexOf(searchString) >= 0) {
 				console.log(document.links[rev].id);
 				resultarr[n] = document.links[rev].id;
-				n++;
+				n++; // If there's an ID, tell the console and increment the array key.
 			}
 			else {
-				console.log("nope (id not a match)\n");
+				console.log("nope (id not a match)\n"); 
+				// If the ID doesn't match, tell console and let the loop take you to the next one.
 			}
 		}
 	}
-	return resultarr;
+	return resultarr; // Ensures the result array gets into global scope for clickLoop to handle it.
 }
 
 function clickLoop() {
-	ii = resultarr.length;
-	for (rev = 0; rev < ii; rev++){
+	ii = resultarr.length; // Set counter for number of items in array.
+	for (rev = 0; rev < ii; rev++){ // Select and click on each element in array, and tell the console.
 		console.log("click loop" + " " + (rev + 1));
-		document.getElementById(resultarr[rev]).click();
+		document.getElementById(resultarr[rev]).click(); 
 		console.log(resultarr[rev] + " clicked #" + (rev + 1) + "/" + ii);
 	}
 }
